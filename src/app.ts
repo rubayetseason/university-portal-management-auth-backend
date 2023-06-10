452afc5dc22484ef4ccc81343cf9668491b6d957
rubayet-islam <season.ss2001@gmail.com>
2023-06-10 23:06:46 +0600

    Created globalErrorHandler function to create generic error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
+import express, { Application } from 'express'
 import usersRouter from './app/modules/users/users.route'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 const app: Application = express()
 
 app.use(cors())
@@ -11,9 +12,15 @@ app.use(express.urlencoded({ extended: true }))
 //Application routes
 app.use('/api/v1/users/', usersRouter)
 
-//Testing route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working successfully')
-})
+// //Testing route
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//   //error throw type -1
+//   throw new ApiError(400, 'Working successfully')
+//   //error throw type -2
+//   next('Next er Error')
+// })
+
+//global error handler
+app.use(globalErrorHandler)
 
 export default app

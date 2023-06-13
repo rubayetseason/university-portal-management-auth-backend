fd26ac0d5d412ecf46d7b6f3d600f8b6fade2c9c
rubayet-islam <season.ss2001@gmail.com>
2023-06-13 16:55:09 +0600

    fixed issues with globalErrorHandler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,22 +1,16 @@
-import { NextFunction, Request, Response } from 'express'
-import { error } from 'winston'
+import { ErrorRequestHandler } from 'express'
 import config from '../../config'
 import ApiError from '../../errors/ApiError'
 import handleValiadtionError from '../../errors/handleValidationError'
 import { IGenericErrorMessage } from '../../interfaces/error'
 
-const globalErrorHandler = (
-  err,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   let statusCode = 500
   let message = 'Something went wrong.'
   let errorMessages: IGenericErrorMessage[] = []
 
-  if (err?.name === 'ValidationError') {
-    const simplifiedError = handleValiadtionError(err)
+  if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValiadtionError(error)
     statusCode = simplifiedError.statusCode
     message = simplifiedError.message
     errorMessages = simplifiedError.errorMessages
@@ -47,7 +41,7 @@ const globalErrorHandler = (
     success: false,
     message,
     errorMessages,
-    stack: config.env !== 'production' ? err?.stack : undefined,
+    stack: config.env !== 'production' ? error?.stack : undefined,
   })
 
   next()

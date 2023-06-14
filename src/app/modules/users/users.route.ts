bafed85c03f924d83a37b07d3f4f8a81923a52e9
rubayet-islam <season.ss2001@gmail.com>
2023-06-14 23:02:33 +0600

    created validateRequest middleware and user validation function for zod handling

--- src/app/modules/users/users.route.ts
+++ src/app/modules/users/users.route.ts
@@ -1,8 +1,14 @@
 import express from 'express'
+import validateRequest from '../../middlewares/validateRequest'
 import { UserController } from './users.controller'
+import { UserValidation } from './users.validation'
 
 const router = express.Router()
 
-router.post('/create-user', UserController.createUser)
+router.post(
+  '/create-user',
+  validateRequest(UserValidation.createUserZodSchema),
+  UserController.createUser
+)
 
 export const UserRoutes = router

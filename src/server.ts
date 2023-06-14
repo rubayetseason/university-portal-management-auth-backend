dcc790fff3c0ac4b96cbbbe445929739d06400df
rubayet-islam <season.ss2001@gmail.com>
2023-06-14 22:01:40 +0600

    [DONE] gracefully closed server for unhandled error

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,33 @@
+import { Server } from 'http'
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
 import { errorlogger, logger } from './shared/logger'
 
-async function server() {
+async function serverFunction() {
+  let server: Server
+
   try {
     await mongoose.connect(config.database_url as string)
     logger.info('🗃️ database connection successful')
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       logger.info(`Application listening on port ${config.port}`)
     })
   } catch (err) {
     errorlogger.error('failed to connect database', err)
   }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 
-server()
+serverFunction()

08fef5ecd3da17738cb333f62106ac84d12b3de3
rubayet-islam <season.ss2001@gmail.com>
2023-06-14 21:52:38 +0600

    [DONE] fixed logger issue

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-expressions */
 import { ErrorRequestHandler } from 'express'
 import config from '../../config'
 import ApiError from '../../errors/ApiError'
 import handleValiadtionError from '../../errors/handleValidationError'
 import { IGenericErrorMessage } from '../../interfaces/error'
+import { errorlogger } from '../../shared/logger'
 
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  config.env === 'development'
+    ? console.log('globalErrorHandler ~', error)
+    : errorlogger.error('globalErrorHandler ~', error)
+
   let statusCode = 500
   let message = 'Something went wrong.'
   let errorMessages: IGenericErrorMessage[] = []

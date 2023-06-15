603c278c059fa92c036965479951bcdf9e5cdc61
rubayet-islam <season.ss2001@gmail.com>
2023-06-15 14:43:14 +0600

    [DONE] created zodErrorHandler and completed zod validation error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-expressions */
 import { ErrorRequestHandler } from 'express'
+import { ZodError } from 'zod'
 import config from '../../config'
 import ApiError from '../../errors/ApiError'
 import handleValiadtionError from '../../errors/handleValidationError'
+import handleZodError from '../../errors/handleZodError'
 import { IGenericErrorMessage } from '../../interfaces/error'
 import { errorlogger } from '../../shared/logger'
 
@@ -17,6 +19,11 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
 
   if (error?.name === 'ValidationError') {
     const simplifiedError = handleValiadtionError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error)
     statusCode = simplifiedError.statusCode
     message = simplifiedError.message
     errorMessages = simplifiedError.errorMessages

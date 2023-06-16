5896ed0ad65799c9811da0aa6e05922a5550f02e
rubayet-islam <season.ss2001@gmail.com>
2023-06-16 16:57:15 +0600

    optimized route throughout the application

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
-import { UserRoutes } from './app/modules/users/users.route';
+import routes from './app/routes';
 const app: Application = express();
 
 app.use(cors());
@@ -11,8 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Application routes
-app.use('/api/v1/users/', UserRoutes);
-app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes);
+app.use('/api/v1/', routes);
 
 // //Testing route
 // app.get('/', (req: Request, res: Response, next: NextFunction) => {
---
+++ src/app/routes/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+import { UserRoutes } from '../modules/users/users.route';
+const router = express.Router();
+
+const moduleRoutes = [
+  { path: '/users/', route: UserRoutes },
+  { path: '/academic-semesters/', route: AcademicSemesterRoutes },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.route));
+
+export default router;

ee8297262ecf2d9ac1a7eeea006e4c3b1597e6e1
rubayet-islam <season.ss2001@gmail.com>
2023-06-22 22:30:29 +0600

    [DONE] created sortOrder and sortBy filtering

--- src/shared/paginationHelper.ts
+++ src/shared/paginationHelper.ts
@@ -1,12 +1,18 @@
+import { SortOrder } from 'mongoose';
+
 type IOptions = {
   page?: number;
   limit?: number;
+  sortBy?: string;
+  sortOrder?: SortOrder;
 };
 
 type IOptionsResults = {
   page: number;
   limit: number;
   skip: number;
+  sortBy: string;
+  sortOrder: SortOrder;
 };
 
 const calculatePagination = (options: IOptions): IOptionsResults => {
@@ -15,10 +21,15 @@ const calculatePagination = (options: IOptions): IOptionsResults => {
 
   const skip = (page - 1) * limit;
 
+  const sortBy = options.sortBy || 'createdAt';
+  const sortOrder = options.sortOrder || 'desc';
+
   return {
     page,
     limit,
     skip,
+    sortBy,
+    sortOrder,
   };
 };
 
